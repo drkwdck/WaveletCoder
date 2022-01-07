125ce72b2eea9aef74e2701cd86b0b597639c0c4
drkwdck <reshetnikov.e.a@mail.ru>
2022-01-07 12:42:46 +0300

    убрал из гитал результаты работы кодера

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 sh ./build.sh
-./app e images/lena_gray_512.tif encode_result.tif
-./app d encode_result.tif decode_result.tif
-rm encode_result.tif
+./app e images/lena_gray_512.tif coder_outputs/encode_result.tif
+./app d encode_result.tif coder_outputs/decode_result.tif
+rm coder_outputs/encode_result.tif
